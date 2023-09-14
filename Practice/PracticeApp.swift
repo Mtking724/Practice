7403d489e977274e7bc07150751ed23969d52d11
motoki0724 <motokiokayasu@gmail.com>
2023-09-14 22:45:50 +0900

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Motoki Okayasu on 2023/09/14.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
