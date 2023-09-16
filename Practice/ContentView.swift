bf958c4180f2505070ed0c9332831a529ed98073
motoki0724 <motokiokayasu@gmail.com>
2023-09-16 21:54:06 +0900

    Hstack  Practice

--- Practice/ContentView.swift
+++ Practice/ContentView.swift
@@ -7,27 +7,47 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State var str = "Hello, SwiftUI"
-    
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text(str)
-            Button("ボタン") {
-                print("ボタンが押されたよ")
-                str = "ハロー, SwiftUI"
+//struct ContentView: View
+//
+//    @State var str = "Hello, SwiftUI"
+//
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text(str)
+//            Button("ボタン") {
+//                print("ボタンが押されたよ")
+//                str = "ハロー, SwiftUI"
+//            }
+//            .padding()
+//        }
+//        .padding()
+//    }
+   
+    struct ContentView: View {
+        var body: some View {
+            HStack {
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(width: 150, height: 200)
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(width: 150, height: 150)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 100, height: 100)
             }
-            .padding()
         }
-        .padding()
     }
-}
+    
+  
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
